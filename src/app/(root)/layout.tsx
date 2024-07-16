39e076a537ba84f0e767892eda09172b26df2679
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-07-16 22:06:22 +0530

    topbar and assets

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,12 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Threadz",
-	description: "Clone of the thread",
+	description: "Clone of the thread using next js",
 };
 
 export default function RootLayout({
@@ -16,10 +20,23 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
 			<ClerkProvider>
-				<body className={inter.className}>{children}</body>
-			</ClerkProvider>
+			<html lang="en">
+				
+				<body className={inter.className}>
+					<Topbar/>
+					<main>
+						<LeftSidebar />
+						<section className="main-container">
+							<div className="w-full max-w-4xl">
+								{children}
+							</div>
+						</section>
+						<RightSidebar />
+					</main>
+					<Bottombar />
+				</body>
 		</html>
+			</ClerkProvider>
 	);
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,9 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <UserButton />
+      <h1 className="head-text text-left">
+        
+      </h1>
     </div>
   );
 }
