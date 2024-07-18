11802a3c936b93a0b394dbf56b7bfc5d6835df5e
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-07-18 10:29:26 +0530

    shadcn ui

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
 	children,
@@ -6,9 +11,9 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en">	
 			<ClerkProvider>
-				<body>{children}</body>
+				<body className={inter.className}>{children}</body>
 			</ClerkProvider>
 		</html>
 	);
